2454924c99516abc9fdadf2cb98f337b1cf0441a
tankistkebab <adrianstefandinca@yahoo.com>
2025-04-25 14:50:05 +0300

    Added a message to main

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+
 
 int main(void)
 {
-    printf("Hello, World!\n");
+    int n, i;
+    printf("Hello Niagra Fall!\nDe cate ori sa repet un mesaj?");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("Miaw Miaw\n");
+    }
+
+
     return 0;
 }
